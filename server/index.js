db3312c06fb52fc848f15227eee595da09f54451
akshmohan <145959795+akshmohan@users.noreply.github.com>
2024-11-13 11:37:03 +0530

    Connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect("mongodb+srv://akshaymohancareer:pPuZBUYMqVwkRpU2@resumes.m78rj.mongodb.net/Resumes?retryWrites=true&w=majority&appName=Resumes").then(() => {
+    console.log("MongoDB CONNECTED");
+}).catch((err) => {
+    console.log("ERROR: ", err);
+});
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("SERVER IS RUNNING ON PORT 3000!!");
 });
+
